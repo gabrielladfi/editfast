bdc5bc19a8453255b8505d54fabdc257014b4284
gabrielladfi <gerencia@ladfilab.com>
2024-11-01 17:39:37 -0500

    first commit

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ API_URL = "http://161.35.113.72:3000/api/v1/prediction/940107e9-61e5-43dd-bf44-a
 headers = {"Authorization": "Bearer OCtTClr7ajlz70NqRwoEWPXMljs7acxZKv54mpH-QAU"}
 
 def query(payload):
+    print(payload)
     response = requests.post(API_URL, headers=headers, json=payload)
+    print(response)
+    print(response.json())
     return response.json()
 
 
